74e8041801606df6701962b3aaaeb7ec6b6491bc
randhirkushwaha93 <randhir.3023@gmail.com>
2018-09-06 09:32:57 +0530

    Initial Commit

---
+++ CoreSpotLightDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreSpotLightDemo
+//
+//  Created by Randhir raj on 9/6/18.
+//  Copyright © 2018 Randhir raj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
