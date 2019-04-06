389d5f85f17bb18807b8df00902e3e03c314c3a4
randhirkushwaha93 <randhir.3023@gmail.com>
2019-04-06 09:51:34 +0530

    provide support swift 4.2

---
+++ CoreSpotLightDemo/ApplicationConstantClass.swift
@@ -0,0 +1,13 @@
+//
+//  ApplicationConstantClass.swift
+//  CoreSpotLightDemo
+//
+//  Created by Randhir raj on 4/6/19.
+//  Copyright © 2019 Randhir raj. All rights reserved.
+//
+
+import UIKit
+
+class ApplicationConstantClass: NSObject {
+
+}
